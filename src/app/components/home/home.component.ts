8e2afaceed912777fc6d4912bf5c9ddf6a46a2fd
caroline <carool@live.com>
2024-01-30 22:36:30 -0300

    feature: adjusts login auth

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Validators, FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { CardModule } from 'primeng/card';
+import { UserService } from '../../services/user/user.service';
+import { AuthRequest } from '../../models/interfaces/user/auth/AuthRequest';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-home',
@@ -21,10 +25,33 @@ export class HomeComponent {
   name!: string;
   emailSignup!: string;
   passwordSignup!: string;
+  
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private cookie: CookieService
+  ) {}
+
 
   onSubmitLoginForm(): void {
-    console.log('Dados do formulário de login:', { email: this.email, password: this.password });
+    const authRequest: AuthRequest = {
+      email: this.email,
+      password: this.password
+    };
+  
+    this.userService.authUser(authRequest).subscribe({
+      next: (response) => {
+        // Aqui você pode armazenar o JWT TOKEN em cookies ou local storage conforme sua preferência
+        this.cookie.set('USER_INFO', response.token);
+        this.router.navigate(['/dashboard']); // Redireciona para a página de dashboard
+      },
+      error: (error) => {
+        // Tratamento de erro
+        console.error('Erro de autenticação', error);
+      }
+    });
   }
+  
 
   onSubmitSignupForm(): void {
     console.log(
