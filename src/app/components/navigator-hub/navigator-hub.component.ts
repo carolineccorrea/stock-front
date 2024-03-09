a76c44e7ed8a8f293d2d2e6d275e62f32add8019
‘’Caroline <carool@live.com>
2024-03-09 15:31:10 -0300

    feature: create navigator hub

--- src/app/components/navigator-hub/navigator-hub.component.ts
+++ src/app/components/navigator-hub/navigator-hub.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { CardModule } from 'primeng/card';
 import { ToastModule } from 'primeng/toast';
+import { Router } from '@angular/router';
 import { ToolbarNavigationComponent } from '../../shared/toolbar-navigation/toolbar-navigation.component';
 
 @Component({
@@ -14,8 +15,14 @@ import { ToolbarNavigationComponent } from '../../shared/toolbar-navigation/tool
   ],
   providers: [MessageService],
   templateUrl: './navigator-hub.component.html',
-  styleUrl: './navigator-hub.component.scss'
+  styleUrls: ['./navigator-hub.component.scss']
 })
 export class NavigatorHubComponent {
+  constructor(private router: Router, private messageService: MessageService) {}
 
+  navigate(path: string) {
+    this.router.navigate([path]);
+    // Exemplo de uso do MessageService
+    this.messageService.add({severity:'info', summary:'Navegação', detail:`Navegando para ${path}`});
+  }
 }
