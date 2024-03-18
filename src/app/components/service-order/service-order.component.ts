00cb2fa7975a9507d53a545cd5bee757a3ac854a
caroline <carool@live.com>
2024-03-18 18:16:42 -0300

    feature: add feature get alls services orders

--- src/app/components/service-order/service-order.component.ts
+++ src/app/components/service-order/service-order.component.ts
@@ -15,6 +15,8 @@ import { CommonModule } from '@angular/common';
 import { DropdownModule } from 'primeng/dropdown';
 import { TableModule } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 
 @Component({
   selector: 'app-service-order',
@@ -33,7 +35,9 @@ import { DialogModule } from 'primeng/dialog';
     AutoCompleteModule,
     DropdownModule,
     TableModule,
-    DialogModule
+    DialogModule,
+    ScrollPanelModule,
+    CommonModule
   ],
   providers: [MessageService],
 })
@@ -56,9 +60,11 @@ export class ServiceOrderComponent {
   selectedCustomer: Customer | null = null;
   previousOrders: ServiceOrder[] = [];
   showOptions: boolean = false;
+  isWarrantyYesEnabled: boolean = false;
   selectedOrder!: any;
 
   selectedCustomerId!: string;
+  selectedOrderId!: string;
 
   constructor(
     private serviceOrderService: ServiceOrderService, 
@@ -111,18 +117,20 @@ export class ServiceOrderComponent {
 
   onOptionSelect(option: Customer): void {
     if (!option.id) {
-      this.messageService.add({
-        severity: 'error',
-        summary: 'Erro',
-        detail: 'Cliente selecionado não possui um ID válido.'
-      });
-      return;
+        this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Cliente selecionado não possui um ID válido.'
+        });
+        return;
     }
-  
-    this.serviceOrder.customerId = option.id; // Correção para garantir que o ID do cliente seja atribuído
-    this.selectedCustomer = option; // Para exibição ou uso adicional
-    this.showOptions = false; // Supondo que isso feche a lista de opções
-  }
+
+    this.serviceOrder.customerId = option.id;
+    this.selectedCustomer = option;
+    this.showOptions = false;
+    this.isWarrantyYesEnabled = true; // Habilita o botão de garantia "Sim"
+}
+
   
   searchCustomer(event: any) {
     this.customerService.searchCustomers(event.query).subscribe(data => {
@@ -149,22 +157,36 @@ export class ServiceOrderComponent {
 
   removeSelectedCustomer(): void {
     this.selectedCustomer = null;
-    this.serviceOrder.customerId = ''; // Limpe o ID do cliente na ordem de serviço
-  }
+    this.serviceOrder.customerId = ''; // Limpa o ID do cliente na ordem de serviço
+    this.serviceOrder.underWarranty = false; // Redefine o estado da garantia para "Não"
+    this.previousOrders = []; // Limpa as ordens de serviço anteriores
+    this.isWarrantyYesEnabled = false; // Desabilita o botão "Sim" da garantia
+    this.selectedOrderId = ''; // Se você estiver mantendo o rastreamento do ID da ordem de serviço selecionada, limpe-o também
+}
 
-  onWarrantyChange(value: boolean): void {
-    this.serviceOrder.underWarranty = value;
-  
-    if (this.serviceOrder.underWarranty) {
-      this.serviceOrderService.getServiceOrdersByCustomerId(this.serviceOrder.customerId)
-        .subscribe(orders => {
-          this.previousOrders = orders;
-        });
-    } else {
-      this.previousOrders = []; // Limpa as ordens anteriores se underWarranty é false
+
+  onWarrantyYesClick(): void {
+    console.log('Warranty "Yes" clicked');
+    // Aqui você pode adicionar qualquer lógica que deseja executar quando "Sim" for selecionado
+    // Por exemplo, disparar a busca por ordens de serviço se o ID do cliente estiver disponível
+    if (this.serviceOrder.customerId) {
+      console.log(`Fetching service orders for customer ID: ${this.serviceOrder.customerId}`);
+      this.fetchServiceOrdersByCustomerId();
     }
   }
-   
+
+  fetchServiceOrdersByCustomerId(): void {
+    this.serviceOrderService.getServiceOrdersByCustomerId(this.serviceOrder.customerId)
+      .subscribe({
+        next: (orders) => {
+          this.previousOrders = orders;
+          // Não precisa transformar os dados para o dropdown
+        },
+        error: (error) => {
+          // Tratamento de erro
+        }
+      });
+  }
 
 }
 
